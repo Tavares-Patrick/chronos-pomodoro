07819c7fa7df9599bace2b4d957ad47d04d5cce0
Patrick Tavares <patrick.t2@hotmail.com>
2025-07-07 16:44:57 -0300

    taskmodel

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -7,16 +7,17 @@ import type { TaskModel } from "../../models/TaskModel";
 import { useTaskContext } from "../contexts/TaskContexts/useTaskContext";
 import { getNextCycle } from "../../utils/getNextCycle";
 import { getNextCycleType } from "../../utils/getNextCycleType";
-import { formartSecondsToMinutes } from "../../utils/formatSecondsToMinutes";
+import { TaskActionTypes } from "../contexts/TaskContexts/taskActions";
+import { Tips } from "../Tips/indext";
 
 export function MainForm() {
-  const { state, setState} = useTaskContext();
+  const { state, dispatch} = useTaskContext();
   const taskNameInput = useRef<HTMLInputElement>(null);
 
   // cycles 
   const nextCycle = getNextCycle(state.currentCycle);
   const nextCycleType = getNextCycleType(nextCycle)
-
+  
   function handleCreateNewTask (event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
@@ -38,23 +39,12 @@ export function MainForm() {
       duration: state.config[nextCycleType],
       type: nextCycleType,
     };
-
-    const secondsRemaining = newTask.duration * 60
-
-    setState(prevState => {
-      return {
-        ...prevState,
-        config: { ...prevState.config},
-        activeTask: newTask,
-        currentCycle: nextCycle,
-        secondsRemaining,
-        formattedSecondsRemaining: formartSecondsToMinutes(secondsRemaining),
-        tasks: [...prevState.tasks, newTask],
-      };
-    });
+    
+    dispatch({ type: TaskActionTypes.START_TASK, payload: newTask});
   }
 
   function handleInterruptTask () {
+   /*
     setState(prevState => {
       return {
         ...prevState,
@@ -68,7 +58,7 @@ export function MainForm() {
           return task;
         })
       };
-    });
+    });*/
   }
 
   return (
@@ -85,7 +75,7 @@ export function MainForm() {
       </div>
 
       <div className='formRow'>
-        <p>Próximo intervalo é de 25min</p>
+        <Tips />
       </div>
       {state.currentCycle > 0 && (
         <div className='formRow'>
--- src/components/contexts/TaskContexts/taskActions.ts
+++ src/components/contexts/TaskContexts/taskActions.ts
@@ -4,9 +4,19 @@ import type { TaskModel } from "../../../models/TaskModel";
 export const TaskActionTypes = {
   START_TASK: 'START_TASK',
   INTERRUPT_TASK: 'INTERRUPT_TASK',
+  RESET_STATE: 'RESET_STATE',
 } as const;
 
+
 // Tipo de ações
-export type TaskActionModel =
+export type TaskActionsWithPayLoad =
   | { type: typeof TaskActionTypes.START_TASK; payload: TaskModel }
+  
+
+export type TaskActionsWithoutPayLoad = 
+  | { type: typeof TaskActionTypes.RESET_STATE }
   | { type: typeof TaskActionTypes.INTERRUPT_TASK };
+
+export type TaskActionModel =
+  | TaskActionsWithPayLoad
+  | TaskActionsWithoutPayLoad;
